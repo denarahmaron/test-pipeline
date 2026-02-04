96e119ff5d647c82741c7136bb44e98355524345
denarahmaron <denarahmaron1@gmail.com>
2026-02-04 22:23:00 +0700

    feat: add CI Pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log(Hello, World!);
+console.log(Learnd CI/CD Pipeline);
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Starting Test...");
+setTimeout("Waiting 3 second", 3000);
+console.log("Test Completed");
